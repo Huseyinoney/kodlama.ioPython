b2950f162b8fd8631f1b279b1837c8eaa40e6bdf
Hüseyin <87951691+Huseyinoney@users.noreply.github.com>
2023-04-06 13:20:10 +0300

    Add files via upload

---
+++ constants/globalConstant.py
@@ -0,0 +1,6 @@
+base_URL = "https://www.saucedemo.com"
+loginButtonContainer = '//*[@id="login_button_container"]/div/form/div[3]/h3'
+testUserNameNullErrorMessage = "Epic sadface: Username is required"
+testPasswordNullErrorMessage = 'Epic sadface: Password is required'
+testInvalidLoginErrorMessage = 'Epic sadface: Sorry, this user has been locked out.'
+testUserNameAndPasswordNotMatch = 'Epic sadface: Username and password do not match any user in this service'
